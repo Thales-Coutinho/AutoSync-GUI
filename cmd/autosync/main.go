fc9d0af2dedf1de0266f0992e437f41255947536
Thales Coutinho <thales9696@protonmail.com>
2025-05-14 16:23:25 -0300

    Restar of the project

---
+++ cmd/autosync/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("AutoSync - Backup")
+	icon, _ := fyne.LoadResourceFromPath("assets/icon.png")
+	w.SetIcon(icon)
+
+	label := widget.NewLabel("")
+
+	dirLabel := widget.NewLabel("")
+
+	selectButton := widget.NewButton("Select Directory", func() {
+		dialog.NewFolderOpen(func(uri fyne.ListableURI, err error) {
+			if err == nil && uri != nil {
+				dirLabel.SetText(uri.Path())
+			}
+		}, w).Show()
+	})
+
+	button := widget.NewButton("Backup", func() {
+		label.SetText("Status: Backup in progress...")
+	})
+
+	content := container.NewVBox(
+		label,
+		dirLabel,
+		selectButton,
+		button,
+	)
+
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(800, 600))
+	w.ShowAndRun()
+}
